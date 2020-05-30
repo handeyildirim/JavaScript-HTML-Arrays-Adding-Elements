5d0f5b0e6333ca62cbdf5bd742aebf1578470c33
Hande Yıldırım <38842051+handeyildirim@users.noreply.github.com>
2020-05-30 17:10:27 +0300

    Add files via upload

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Arrays - Add Fruit
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/handeyildirim/pen/YzymryL](https://codepen.io/handeyildirim/pen/YzymryL).
+
+
---
+++ script.js
@@ -0,0 +1,14 @@
+var fruits = ["Banana", "Orange", "Apple", "Mango"] ;
+
+function loadFruits(){
+  document.getElementById("fruits").innerHTML = fruits;
+}
+
+function myFunction(){
+  //ask to user's fav fruit
+  var fruit = prompt("What is your favorite fruit?");
+  <!--to add new fruit we have to write array name(furits[]) firstly, then the index(fruits.length). Here, fruits.length is equal to 4. So, the next fruit will be added to the 4th index-->
+  <!-- we can add more fruits with this method-->
+  fruits[fruits.length] = fruit;
+  document.getElementById("fruits").innerHTML = fruits;
+}
