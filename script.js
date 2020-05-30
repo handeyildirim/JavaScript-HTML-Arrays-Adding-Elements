fb7174b090a7040e1e3efc38e5b59216bbd17183
Hande Yıldırım <38842051+handeyildirim@users.noreply.github.com>
2020-05-30 17:13:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ function loadFruits(){
 function myFunction(){
   //ask to user's fav fruit
   var fruit = prompt("What is your favorite fruit?");
-  <!--to add new fruit we have to write array name(furits[]) firstly, then the index(fruits.length). Here, fruits.length is equal to 4. So, the next fruit will be added to the 4th index-->
-  <!-- we can add more fruits with this method-->
+  //to add new fruit we have to write array name(furits[]) firstly, then the index(fruits.length). 
+  //Here, fruits.length is equal to 4. So, the next fruit will be added to the 4th index.
+  //We can add more fruits with this method
   fruits[fruits.length] = fruit;
   document.getElementById("fruits").innerHTML = fruits;
 }
